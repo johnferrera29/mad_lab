46caf44b01745d6990faeb5f66e07c5bef074d92
John Rolan Ferrera <john.ferrera29@gmail.com>
2023-11-15 15:02:36 +0800

    Add freeze ray (#4)
    * Add freezable component
    
    * Add freeze ray entity
    
    * Fix player not snapping to moving platforms on idle state

--- shared_scripts/components/interactable_object.gd
+++ shared_scripts/components/interactable_object.gd
@@ -1,5 +1,5 @@
 class_name InteractableObject
-extends Node
+extends CollisionObject2D
 ## Virtual base class that exposes different types of interactable components.
 ##
 ## Every interactable object that wants to be detected by [TargetingSystem] should inherit this class.
@@ -8,3 +8,4 @@ extends Node
 @export_group("Components")
 @export var scalable_component: ScalableComponent
 @export var anchor_component: AnchorComponent
+@export var freezable_component: FreezableComponent
