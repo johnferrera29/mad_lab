b4e8312ae7363eeccfe65cb192dde0debe80b816
John Rolan Ferrera <john.ferrera29@gmail.com>
2023-11-17 15:35:17 +0800

    Refactor freezable component

--- shared_scripts/interactable_components/freezable_component.gd
+++ shared_scripts/interactable_components/freezable_component.gd
@@ -9,18 +9,30 @@ extends Node
 @export var target: Node2D
 ## The target's [Sprite2D] or [AnimatedSprite2D].
 ## Sprite will be highlighted when interacting with [TargetingSystem].
-## If sprite is an [AnimatedSprite2D], will stop animation when frozen.
 @export var sprite: Node2D
-## Target's [AnimationPlayer] if any. Will stop animation when  frozen.
-@export var animator: AnimationPlayer
+## Node that handles the target's animation, if any.
+## Currently supports [AnimatedSprite2D] and [AnimationPlayer].
+@export var animator: Node
 ## Time it takes to before thawing out in seconds.
 ## During this time, [method _process] and [method _physics_process] callbacks will be stopped.
 @export_range(0.0, 99.0, 0.1) var freeze_time: float
 
 ## Flag to prevent double freezing.
-var is_frozen: bool
+var _is_frozen: bool
 
 
-func _ready() -> void:
-	# Validate the export variables.
-	assert(sprite is Sprite2D or sprite is AnimatedSprite2D, "Export variable [sprite] must be of Sprite2D or AnimatedSprite2D")
+# TODO: Toggle built-in processing. Currently only toggling animator.
+func freeze() -> void:
+	if _is_frozen: return
+
+	print("freeze")
+	_is_frozen = true
+	Utils.ProcessUtils.toggle_processing(animator, false)
+	
+	await get_tree().create_timer(freeze_time).timeout
+	
+	print("unfreeze")
+	_is_frozen = false
+	Utils.ProcessUtils.toggle_processing(animator, true)
+	
+	# TODO: Add frozen shader effect.
