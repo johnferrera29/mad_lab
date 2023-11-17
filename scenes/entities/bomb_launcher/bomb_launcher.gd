5d4175e800797023fe2a79ff929a08ab4cfaedef
John Rolan Ferrera <john.ferrera29@gmail.com>
2023-11-17 12:57:03 +0800

    Refactor bomb launcher
    * Use components instead of inheritance for most of the weapon functionality
    * Cleanup code

--- scenes/entities/bomb_launcher/bomb_launcher.gd
+++ scenes/entities/bomb_launcher/bomb_launcher.gd
@@ -1,20 +1,29 @@
 class_name BombLauncher
-extends Launcher
+extends Weapon
 ## A bomb launcher... that launches bombs. Launches a [BombProjectile] towards a target position.
 ##
 ## Target object must extend an [InteractableObject] and contain a [BreakableComponent].
 
 
 ## Determines the spawn position and rotation of the projectile.
-@onready var projectile_spawn_point := $ProjectileSpawnPoint as Marker2D
 @onready var targeting_system := $TargetingSystem as TargetingSystem
+@onready var projectile_launcher := $ProjectileLauncher as ProjectileLauncher
+@onready var projectile_spawn_point := $ProjectileSpawnPoint as Marker2D
 
 
-func _on_targeting_system_target_locked(target_position: Vector2) -> void:
-	var projectile_velocity := launch_speed * global_position.direction_to(target_position)
-	var projectile := create_projectile(projectile_spawn_point.global_position, projectile_spawn_point.rotation, projectile_velocity, projectile_lifespan)
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("interact"):
+		print("Launch bomb!")
+		var target_position := get_global_mouse_position()
+		var projectile_velocity := projectile_launcher.launch_speed * global_position.direction_to(target_position)
+		var projectile := projectile_launcher.create_projectile(
+			projectile_spawn_point.global_position,
+			projectile_spawn_point.rotation,
+			projectile_velocity,
+			projectile_launcher.projectile_lifespan
+		)
 	
-	launch_projectile(projectile)
+		projectile_launcher.launch_projectile(projectile)
 
 
 func _on_targeting_system_target_detected(target: InteractableObject) -> void:
