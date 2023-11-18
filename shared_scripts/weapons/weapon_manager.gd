5a3d3d44179c924de0420d40eff051af299ca055
John Rolan Ferrera <john.ferrera29@gmail.com>
2023-11-18 15:42:51 +0800

    Add methods and public variables to interact with weapon manager

--- shared_scripts/weapons/weapon_manager.gd
+++ shared_scripts/weapons/weapon_manager.gd
@@ -1,25 +1,38 @@
 class_name WeaponManager
 extends Node2D
-## Class that handles switching between different types of weapon.
+## Class that handles switching between different availble weapons.
 
+
+## Determines the scroll direction when calling [method scroll_through_weapons].
+enum SCROLL_DIRECTION {
+	NEXT = 1,
+	PREVIOUS = -1
+}
 
 @export var initial_weapon: Weapon
 
+## Currently selected weaon.
 var current_weapon: Weapon
-var weapons: Dictionary = {}
+## A dictionary of weapons inside Weapon Manager. Node's lowercased name is used as the key.
+var weapon_dictionary: Dictionary = {}
+## An array of weapons. Contains the same weapons as [member weapon_dictionary] but in array form.
+## The order of the array is based on the child's order in the editor.
+var weapon_list: Array[Weapon]
 
 
 func _ready() -> void:
 	for child in get_children():
 		if child is Weapon:
-			weapons[child.name.to_lower()] = child
+			weapon_dictionary[child.name.to_lower()] = child
+			weapon_list.append(child as Weapon)
 			child.disable_weapon()
 		else:
-			push_warning("Weapon Manager contains a child node that is not derived from virtual Weapon class.")
+			push_warning("Weapon Manager contains a child node that is not derived from Weapon class.")
 	
 	change_weapon(initial_weapon)
 
 
+## Changes currently selected weapon to [param weapon].
 func change_weapon(weapon: Weapon) -> void:
 	if not weapon: return
 	
@@ -28,3 +41,23 @@ func change_weapon(weapon: Weapon) -> void:
 	
 	current_weapon = weapon
 	current_weapon.enable_weapon()
+
+
+## Scroll through list weapons starting from the [member current_weapon] index inside [member weapon_list].
+## Provide optional [param scroll_direction] to determine scrolling direction. Use [enum WeaponManager.SCROLL_DIRECTION].
+## Returns null if [member current_weapon] not set.
+func scroll_through_weapons(scroll_direction: SCROLL_DIRECTION = SCROLL_DIRECTION.NEXT) -> Weapon:
+	if not current_weapon: return
+
+	var max_index := weapon_list.size()
+	var index := weapon_list.find(current_weapon) + scroll_direction
+	
+	# Went over last weapon index.
+	if index >= max_index:
+		index = 0
+	
+	# Wend under first weapon index.
+	if index <= -2:
+		index = weapon_list.size() - 1
+	
+	return weapon_list[index]
