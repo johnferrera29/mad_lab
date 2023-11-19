48c5be2a17581864462c1da9f2f3798db69bccfa
John Rolan Ferrera <john.ferrera29@gmail.com>
2023-11-19 18:07:25 +0800

    Add scene manager autoload

--- shared_scripts/autoloads/signal_bus.gd
+++ shared_scripts/autoloads/signal_bus.gd
@@ -6,4 +6,12 @@ extends Node
 
 # Player related signals.
 signal player_died()
+## Signal emitted once a new respawn point has been set.
+## Accepts [param respawn_position] that determines where the player will respawn.
 signal player_respawn_point_set(respawn_position: Vector2)
+
+
+# Scene related signals.
+## Signal emitted when transitioning between scenes.
+## Use [SceneManager.SceneChangeParams] to create parameters.
+signal scene_change_triggered(params: SceneManager.SceneChangeParams)
