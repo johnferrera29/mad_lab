baa870a4de43a2c62cf2f4081d513052f68f7913
John Rolan Ferrera <john.ferrera29@gmail.com>
2023-11-23 15:38:37 +0800

    Additional targeting system features
    * Add raycast length limit
    * Add default values such as texture, width, etc.

--- scenes/entities/targeting_system/targeting_system.gd
+++ scenes/entities/targeting_system/targeting_system.gd
@@ -12,8 +12,12 @@ signal target_lost(target: InteractableObject)
 ## Makes the raycast line visible.
 @export var visible_raycast_line: bool
 ## Texture to be applied to the visible line. By default no texture is applied.
-## If provided, texture is repeated.
 @export var line_texture: Texture2D
+## The starting point where the line will be drawn.
+@export var line_starting_point: Marker2D
+## The length of the raycast and drawn line.
+## If not provided, will draw a line from [member line_starting_point] to mouse position.
+@export var line_length: float
 ## The width of the visible line.
 @export var line_width: float
 
@@ -38,15 +42,27 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	target_position = get_local_mouse_position()
-	var target_global_position := get_global_mouse_position()
-	
-	target_angle = global_position.angle_to_point(target_global_position)
-	
+	var local_mouse_position := get_local_mouse_position()
+	var global_mouse_position := get_global_mouse_position()
+
+	# Create the raycast line.
 	if visible_raycast_line:
-		_draw_raycast_line(global_position, target_global_position)
+		var starting_position = global_position if not line_starting_point else line_starting_point.global_position
+		_draw_raycast_line(starting_position, global_mouse_position)
+
+		if line_length:
+			target_position = _caculate_end_position(position, local_mouse_position, line_length)
+		else:
+			target_position = local_mouse_position
+	else:
+		target_position = local_mouse_position
 	
+	# Set the target angle
+	target_angle = global_position.angle_to_point(global_mouse_position)
+
 	if is_colliding():
+		# TODO: Redraw raycast if colliding with another object.
+
 		# Check if target extends InteractableObject.
 		var target = get_collider() as InteractableObject
 		if target:
@@ -67,10 +83,12 @@ func toggle_shader_effect(sprite: CanvasItem, flag: bool) -> void:
 
 
 func _init_line() -> void:
-	_line.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
-	_line.texture_mode = Line2D.LINE_TEXTURE_TILE
-	_line.texture = line_texture
 	_line.width = line_width
+
+	if line_texture:
+		_line.texture = line_texture
+		_line.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
+		_line.texture_mode = Line2D.LINE_TEXTURE_TILE
 
 
 func _init_shader_params() -> void:
@@ -78,9 +96,20 @@ func _init_shader_params() -> void:
 		shader_material.set_shader_parameter(key, shader_parameters[key])
 
 
+## Calculate the adjusted end position based on [param length].
+## Useful if we want to limit the length of the raycast or when drawing the line.
+func _caculate_end_position(start: Vector2, end: Vector2, length: float) -> Vector2:
+		var direction := start.direction_to(end)
+		var end_point :=  start + (direction * length)
+
+		return end_point
+
+
 ## Draws a visible line from [param start] to [param end].
 func _draw_raycast_line(start: Vector2, end: Vector2) -> void:
+	var adjusted_end = end if not line_length else _caculate_end_position(start, end, line_length)
+
 	_line.points = PackedVector2Array([
 		start,
-		end
+		adjusted_end
 	])
