85a76e8551389ba3176ab4fcf11c0b6f8877251e
John Rolan Ferrera <john.ferrera29@gmail.com>
2023-11-28 00:50:00 +0800

    Add interactable component sound effects
    * Add explosion sound on object break
    * Add freezing sound effect when object is frozen
    * Add scaling sound effects

--- shared_scripts/autoloads/audio_manager.gd
+++ shared_scripts/autoloads/audio_manager.gd
@@ -1,9 +1,20 @@
 extends Node
+## Audio Manager singleton.
+##
+## Useful for playing one shot audio for dynamically created and freed objects.
+## AudioStreamPlayers will use the SFX audio bus.
 
 
-func play_sound(stream: AudioStream) -> void:
+const SFX_BUS_NAME: String = "SFX"
+
+func play_sound(stream: AudioStream, params: PlaySoundParams = PlaySoundParams.new()) -> void:
 	var audio_stream_player := AudioStreamPlayer.new()
+	audio_stream_player.bus = SFX_BUS_NAME
+
 	audio_stream_player.stream = stream
+	audio_stream_player.volume_db = params.volume_db
+	audio_stream_player.pitch_scale = params.pitch_scale
+	
 	audio_stream_player.finished.connect(_on_audio_stream_player_finished.bind(audio_stream_player))
 
 	add_child(audio_stream_player)
@@ -13,3 +24,9 @@ func play_sound(stream: AudioStream) -> void:
 
 func _on_audio_stream_player_finished(audio_stream_player: AudioStreamPlayer) -> void:
 	audio_stream_player.queue_free()
+
+
+class PlaySoundParams:
+	var volume_db: float = 0.0
+	var pitch_scale: float = 1.0
+
