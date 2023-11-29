018bc096488ceb9a685088be3f391de0882cd2f6
John Rolan Ferrera <john.ferrera29@gmail.com>
2023-11-29 22:06:34 +0800

    Add one shot dialog triggers

--- scenes/environment/triggers/dialog_trigger/dialog_trigger.gd
+++ scenes/environment/triggers/dialog_trigger/dialog_trigger.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 @export var lines: Array[String]
+@export var one_shot: bool
 @export var auto_advance_time: float = 1.0
 
 @onready var trigger := $TriggerArea as TriggerArea
@@ -9,6 +10,7 @@ extends Node2D
 
 func _ready() -> void:
 	trigger.trigger_keys.append(GameManager.player)
+	trigger.one_shot = one_shot
 
 
 func _on_trigger_area_triggered() -> void:
