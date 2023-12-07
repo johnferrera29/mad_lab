d6fd6f5ddc8136f1289d6638c760cab8eff9ba70
John Rolan Ferrera <john.ferrera29@gmail.com>
2023-12-07 16:40:24 +0800

    Remove player grappling state

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -20,7 +20,6 @@ extends CharacterBody2D
 ## Max [member CharacterBody2D.velocity.y] the player can achieve while falling.
 @export var terminal_velocity: float
 
-
 # Jump related variables.
 # @tutorial: https://www.youtube.com/watch?v=IOe1aGY6hXA
 @onready var jump_force: float = (jump_height * 2.0) / jump_time_to_peak
@@ -33,7 +32,6 @@ extends CharacterBody2D
 @onready var run_state := $StateMachine/Run as PlayerRunState
 @onready var air_state := $StateMachine/Air as PlayerAirState
 @onready var attack_state := $StateMachine/Attack as PlayerAttackState
-@onready var grappling_state := $StateMachine/Grappling as PlayerGrapplingState
 
 # Weapon manager.
 @onready var weapon_manager := $WeaponManager as WeaponManager
@@ -72,7 +70,3 @@ func _connect_state_transitions() -> void:
 	attack_state.actor_fell.connect(change_to_fall_state)
 	attack_state.actor_jumped.connect(change_to_jump_state)
 	attack_state.actor_ran.connect(change_to_run_state)
-
-	# Connect player grappling transition states.
-	grappling_state.actor_idle.connect(change_to_idle_state)
-	grappling_state.actor_fell.connect(change_to_fall_state)
